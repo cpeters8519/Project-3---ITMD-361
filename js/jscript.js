00b218571664551d49edec963c09184edbfdc8d9
cpeters8519 <cpeters8519@gmail.com>
2026-03-01 20:10:45 -0600

    added marker and infowindow

--- js/jscript.js
+++ js/jscript.js
@@ -1,11 +1,30 @@
 let map;
+let geocoder;
 
 function initMap() {
-  const hobart = { lat: 41.5561, lng: -87.4316 }; // Hobart, IN
+  const hobart = { lat: 41.5561, lng: -87.4316 };
   map = new google.maps.Map(document.getElementById("map"), {
     center: hobart,
     zoom: 12
+	mapTypeControl: false
   });
+}
+
+geocoder = new google.maps.Geocoder();
+
+const marker = new google.maps.Marker({
+	position: hobart,
+	map: map,
+	title: "Hobart, IN"
+});
+
+const infoWindow = new google.maps.InfoWindow({
+	content: "<h3>Hobart, Indiana</h3><p>My hometown</p>
+});
+
+marker.addListener("click", () => {
+	infoWindow.open({ anchor: marker, map });
+});
 }
 
 window.initMap = initMap;
